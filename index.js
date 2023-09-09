9277b87394b05789ff9249d16c30fc94a5a643ae
Atoti2 <bogyikormi3@gmail.com>
2023-09-09 16:52:02 +0200

    New game function

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const player = document.querySelectorAll("div.player div");
 let computerPoint = document.querySelector(".computerPoints");
 let playerPoint = document.querySelector(".playerPoints");
 let winner = document.querySelector(".winner");
+let win = false
+let result = document.querySelector(".result")
+
+function newGameFunction(){
+  window.location.reload(true);
+}
 
 player.forEach((button) => {
   button.addEventListener("click", getPlayerChoice);
@@ -20,44 +26,56 @@ function disableButtons() {
   });
 }
 
+
+
 function playRound(computerSelection, playerSelection) {
-  let result;
+  win = false
   if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
     playerScore += 1;
-    result = `You win ${computerSelection} beats ${playerSelection}<br>
+    res = `You win ${computerSelection} beats ${playerSelection}<br>
                             Player point: ${playerScore}
                             <br>Computer point: ${compScore}`;
 
 
   } else if (playerSelection == computerSelection) {
-    result = `It's a tie! You both chose ${playerSelection}<br>
+    res = `It's a tie! You both chose ${playerSelection}<br>
         Player point: ${playerScore}
         Computer point: ${compScore}`;
   } else {
     compScore += 1;
-    result = `You lose! ${computerSelection} beats ${playerSelection}<br>
+    res = `You lose! ${computerSelection} beats ${playerSelection}<br>
                             Player point: ${playerScore}
                             <br>Computer point: ${compScore}`;
   }
   checkWinner()
-  result = document.querySelector(".result").innerHTML = result;
+  result.innerHTML = res
   return;
 }
 
 function checkWinner(){
     if(playerScore == 5){
+        win = true
         winner.textContent = "Player wins!";
         disableButtons();
+        if(win){
+          document.querySelector('.newGame').style.visibility = "visible"
+        }
     }
     else if(compScore == 5){
+        win = true
         winner.textContent = "Computer wins!";
         disableButtons();
+        if(win){
+          document.querySelector('.newGame').style.visibility = "visible"
+        }
     }
 }
+
+
 
 function getPlayerChoice(e) {
   let playerSelection = e.target.id;
